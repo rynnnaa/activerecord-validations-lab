07eff66b3ab552950182086e3b8ddb2207ff0a08
Ryna <pinktulips@Hotmail.com>
2021-07-20 11:44:35 -0700

    almost

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,17 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+  message: "pick one" }
   validate :title_sufficiently_clickbait_y
-  validates_with CategoryValidator
-
+  
   def title_sufficiently_clickbait_y
-    if title.!include == "Won't Believe" || "Secret"|| "Top [number]" || "Guess"
-      errors.add(:title, "Has to be a valid title")
+    @titles = ["Won't Believe", "Secret", "Top [number]","Guess"]
+    @titles.each do |t|
+      unless t.present?
+        errors.add(:title, "Has to be a valid title")
+      end
     end
   end
 end
