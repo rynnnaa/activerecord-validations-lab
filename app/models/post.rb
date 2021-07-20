4e409add54b9ca72b1049970db4ed39002d195a3
Ryna <pinktulips@Hotmail.com>
2021-07-20 10:43:27 -0700

    working on it

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validate :title_sufficiently_clickbait_y
+  validates_with CategoryValidator
+
+  def title_sufficiently_clickbait_y
+    if title.!include == "Won't Believe" || "Secret"|| "Top [number]" || "Guess"
+      errors.add(:title, "Has to be a valid title")
+    end
+  end
 end
